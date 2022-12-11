9cc705809691b693418c5be3cd8c7fb2762502a4
hibo2261 <hborse1@gmail.com>
2022-12-11 18:25:53 +0530

    [Himanshu] : Ability to validate the emailId as per the given instruction

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,23 +1,23 @@
 package com.bridgelabz;
 
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
 
 
-    public static void toCheckForLastNameValidation(String LastName){
-       
-        boolean isLastName;
-        String regexName = "^[A-Z]{1}[A-Z a-z]{3,}$";
-        Pattern object = Pattern.compile(regexName);
-        Matcher objectMatcher = object.matcher(LastName);
-        isLastName = objectMatcher.matches();
-        if(isLastName) {
-            System.out.println(LastName + " is an valid name");
+    public static void toCheckForEmailValidation(String email){
+        
+            boolean isEmailId;
+            String emailIdRegex ="^[a-z]*.[a-z]*@[b]+[l]+.[c]+[o]+.[i]+[n]$";//abc.xyz@bl.co.in
+            Pattern patternObj = Pattern.compile(emailIdRegex);
+            Matcher matcherObj = patternObj.matcher(email);
+            isEmailId =  matcherObj.matches();
+
+            if(isEmailId)
+                System.out.println(email+" is a Valid Email Id\n");
+            else
+                System.out.println(email+" is a Invalid Email Id");
         }
-        else{
-            System.out.println(LastName + " is an invalid user name");
-        }
-    }
 }
--- src/com/bridgelabz/UserRegistrationMain.java
+++ src/com/bridgelabz/UserRegistrationMain.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 
 public class UserRegistrationMain {
-   
-	public static void main(String[] args){
+    public static void main(String[] args){
+      
+    	Scanner sc = new Scanner(System.in);
+      
+    	UserRegistration user = new UserRegistration();
         
-		Scanner sc = new Scanner(System.in);
-       
-		UserRegistration user = new UserRegistration();
-        
-		System.out.println("Welcome to the User Registration System");
+    	System.out.println("Welcome to the User Registration System");
 
        
-		System.out.println("Please Enter the lastName:");
        
-		String LastName = sc.nextLine();
+    	System.out.println("Please Enter the emailId:");
        
-		user.toCheckForLastNameValidation(LastName);//UC2
+    	String email = sc.nextLine();
+       
+    	user.toCheckForEmailValidation(email);//UC3
 
 
 
