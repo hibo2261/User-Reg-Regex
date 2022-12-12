4829bb6846b0dcc8bce93d15e469664905b07196
hibo2261 <hborse1@gmail.com>
2022-12-13 00:37:43 +0530

    [Himanshu] : Ability to add Special Character to validate password

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,24 +1,29 @@
 package com.bridgelabz;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-  
-
+    
     public static void toCheckPasswordValidation(String password){
         
-
             boolean isPassword;
-            String mobileNumberRegex = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}";
+           
+            String mobileNumberRegex = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[*.!@#$%^&(){}:;<>?/~_+-=|\\]]).{8,}$";
+          
             Pattern obj = Pattern.compile(mobileNumberRegex);
+          
             Matcher matcherObj = obj.matcher(password);
+           
             isPassword =  matcherObj.matches();
 
+          
             if(isPassword)
-                System.out.println(password+" is a Valid  password\n");
+             
+            	System.out.println(password+" is a Valid  password\n");
+          
             else
-                System.out.println(password+" is a Invalid  password");
+             
+            	System.out.println(password+" is a Invalid  password");
 
         }
 }
